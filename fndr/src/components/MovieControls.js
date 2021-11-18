842219e8217ddf590a0dc8bb7101c792ea95ab66
Michael Nieding <lofdofgutam@googlemail.com>
2021-11-18 20:36:05 +0100

    inital commit :D

---
+++ fndr/src/components/MovieControls.js
@@ -0,0 +1,36 @@
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
+export const MovieControls = ({movie, type}) => {
+    const {removeMovieFromWatchList, addMovieToWatched, moveToWatchList, removeMovieFromWatched} = useContext(GlobalContext);
+    return (
+        <div className="inner-card-controls">
+            {/* WATCHLIST-BUTTONS */}
+            {type === "watchlist" && (
+                <>
+                    {/* BUTTON: MOVE TO WATCHED-LIST */}
+                    <button className="ctrl-btn" onClick={() => addMovieToWatched(movie)}>
+                        <i className="fa-fw far fa-eye"></i>
+                    </button>
+                    {/* BUTTON: REMOVE FROM WATCHLIST */}
+                    <button className="ctrl-btn" onClick={() => removeMovieFromWatchList(movie.id)}> 
+                        <i className="fa-fw fa fa-times"></i>
+                    </button>
+                </>
+            )}
+            {/* WATCHED-BUTTONS */}
+            {type === "watched" && (
+                <>
+                    {/* BUTTON: MOVE BACK TO WATCHLIST */}
+                    <button className="ctrl-btn" onClick={() => moveToWatchList(movie)}>
+                        <i className="fa-fw far fa-eye-slash"></i>
+                    </button>
+                    {/* BUTTON: REMOVE FROM WATCHED */}
+                    <button className="ctrl-btn" onClick={() => removeMovieFromWatched(movie.id)}> 
+                        <i className="fa-fw fa fa-times"></i>
+                    </button>
+                </>
+            )}
+        </div>
+    )
+}
---
+++ fndr/src/components/ResultCard.js
@@ -0,0 +1,53 @@
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
+export const ResultCard = ({movie}) => {
+    const { addMovieToWatchList, addMovieToWatched, watchlist, watched } = useContext(GlobalContext)
+
+    let storedMovie = watchlist.find(object => object.id === movie.id);
+    let storedMovieWatched = watched.find(object => object.id === movie.id);
+
+    const watchlistDisabled = storedMovie ? true : storedMovieWatched ? true: false;
+    const watchedDisabled = storedMovieWatched ? true : false;
+
+    return (
+        <div className="result-card">
+            <div className="poster-wrapper">
+                {movie.poster_path ? (
+                    <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                    alt={`${movie.title} Poster`}
+                    />
+                ) 
+                : 
+                (<div className="filler-poster"></div>) //show a filler Image in case there is no movie poster
+                }
+            </div>
+            <div className="info">
+                <div className="Header">
+                    <h3 className="title">{movie.title}</h3>
+                    <h4 className="release-date">
+                        {movie.release_date ? movie.release_date.substring(0,4) : "-"}
+                    </h4>
+                </div>
+
+                <div className="controls">
+                    <button 
+                    className="btn" 
+                    onClick={() => addMovieToWatchList(movie)}
+                    disabled={watchlistDisabled}
+                    >
+                    Add to WatchList
+                    </button>
+
+                    <button 
+                    className="btn" 
+                    onClick={() => addMovieToWatched(movie)}
+                    disabled={watchedDisabled}
+                    >
+                    Add to Watched
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
